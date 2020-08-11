1986c77850328f1dd1f0e59ad7156f8474487cb0
ResilientSpring <ReamsAggravate0615>
2020-08-11 11:07:28 +0800

    2020/8/10

---
+++ Example/src/CharArithDemo.java
@@ -0,0 +1,22 @@
+// Character variables can be handled like integers. 
+class CharArithDemo {
+
+	public CharArithDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch;
+
+		ch = 'x';
+		System.out.println("ch contains " + ch);
+
+		ch++; // increment ch (A char can be incremented.)
+		System.out.println("ch is now " + ch);
+
+		ch = 90; // give ch the value z (A char can be assigned an integer value.)
+		System.out.println("ch is now " + ch);
+	}
+}
