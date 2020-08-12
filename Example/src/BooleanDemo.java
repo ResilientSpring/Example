d068ca56b250309b3ec47732f720afeda9233684
ResilientSpring <ReamsAggravate0615>
2020-08-12 15:08:22 +0800

    Committed

--- Example/src/BooleanDemo.java
+++ Example/src/BooleanDemo.java
@@ -11,13 +11,13 @@ public class BooleanDemo {
 		boolean b = true;
 		
 		// if(condition) statement;
-		if (b) System.out.println("The output value of the boolean variable is " + b);
+		if (b) System.out.println("The output value of the boolean variable is " + b + ".");
 		
 		// if (condition) statement;
 		if (10>9) System.out.println("This is executed. ");
 		
 		// The outcome of a relational operator, such as <, is a boolean value. 
-		System.out.println(" 10 is larger than 9 is a statement that is " + (10 > 9));
+		System.out.println("10 is larger than 9 is a statement that is " + (10 > 9) + ".");
 
 	}
 
