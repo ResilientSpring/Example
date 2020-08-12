a566a4090e365e63ebe978d3bd8ce61f333a317e
ResilientSpring <ReamsAggravate0615>
2020-08-12 14:38:40 +0800

    2020/8/12

---
+++ Example/src/BooleanDemo.java
@@ -0,0 +1,24 @@
+
+public class BooleanDemo {
+
+	public BooleanDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean b = true;
+		
+		// if(condition) statement;
+		if (b) System.out.println("The output value of the boolean variable is " + b);
+		
+		// if (condition) statement;
+		if (10>9) System.out.println("This is executed. ");
+		
+		// The outcome of a relational operator, such as <, is a boolean value. 
+		System.out.println(" 10 is larger than 9 is a statement that is " + (10 > 9));
+
+	}
+
+}
