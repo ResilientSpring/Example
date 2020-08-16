f06804d7ae501be7db6da15ba5119ed27bec7e21
ted10014 <ted10014@gmail.com>
2020-08-16 13:15:46 +0800

    2020/8/16

---
+++ Example/src/StrDemo.java
@@ -0,0 +1,20 @@
+// Demonstrate escape sequences in strings. 
+public class StrDemo {
+
+	public StrDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Use \n to generate a new line.
+		System.out.println("First line \nSecond line");
+		
+		// Use tabs to align output.
+		System.out.println("A\tB\tC");
+		System.out.println("D\tE\tF");
+
+	}
+
+}
