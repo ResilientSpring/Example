190efc0fbd272cf44d91b309d8281918ae7c15a7
ted10014 <ted10014@gmail.com>
2020-08-25 12:02:10 +0800

    2020/8/25

---
+++ Example/src/IncrementAndDecrement.java
@@ -0,0 +1,37 @@
+
+public class IncrementAndDecrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 12 ; 
+		x++;
+		
+		System.out.println("X = " + x);
+		
+		int y = 23 ; 
+		y = y++;   // y = ( y = y + 1 );
+		
+		System.out.println("Y = " + y);
+		
+		int z = 1972 ; 
+		++z ;
+		
+		System.out.println("Z = " + z);
+		
+		int a = 520 ; 
+		--a;
+		
+		System.out.println("a = " + a);
+		
+		int b = 520 ; 
+		b--; 
+		System.out.println("b = " + b);
+		
+		int c = 520; 
+		c = c--;
+		System.out.println("c = " + c);
+
+	}
+
+}
