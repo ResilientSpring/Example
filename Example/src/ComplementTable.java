8d5a11606551c3443495cd471c15efe81ac8a917
ted10014 <ted10014@gmail.com>
2020-08-28 18:17:42 +0800

    2020/08/28

--- Example/src/ComplementTable.java
+++ Example/src/ComplementTable.java
@@ -1,4 +1,5 @@
-
+// Try this: 2-2: 6. On your own, try modifying the program so that it uses and displays 1's and 0's, rather than 
+// true and false. 
 public class ComplementTable {
 
 	public static void main(String[] args) {
