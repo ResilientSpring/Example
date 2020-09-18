9a3a3c9b505e8efe657cfd42f61f8a6a007ba421
ResilientSpring <ReamsAggravate0615>
2020-09-18 14:11:08 +0800

    2020/09/18

---
+++ Chapter3/WhileDemo.java
@@ -0,0 +1,19 @@
+// Demonstrate the while loop.
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch;
+		
+		// print the alphabet using a while loop
+		ch = 'a';
+		
+		while (ch <= 'z') {
+			System.out.print(ch);
+			ch++;
+		}
+
+	}
+
+}
