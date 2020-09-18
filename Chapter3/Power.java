4cb04e9eb8a1a17fe7e3a4a81e7f9374f99d21af
ResilientSpring <ReamsAggravate0615>
2020-09-18 14:39:54 +0800

    2020/09/18

---
+++ Chapter3/Power.java
@@ -0,0 +1,25 @@
+// Compute integer powers of 2.
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int e, result;
+		
+		for (int i = 0; i < 10; i++) {
+			
+			result = 1;
+			e = i;
+			
+			while (e > 0) {
+				result *= 2;
+				e--;
+			}
+			
+			System.out.println("2 to the " + i + " power is " + result);
+			
+		}
+
+	}
+
+}
