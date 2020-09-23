facadf443ab2eacabc2f6747e1d10783fe351d4c
ted10014 <ted10014@gmail.com>
2020-09-24 01:15:07 +0800

    Committed 2020/9/24

---
+++ Chapter3/Break3.java
@@ -0,0 +1,29 @@
+// Using break with nested loops. 
+public class Break3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < 3; i++) {
+			
+			System.out.println("Outer loop count: " + i);
+			System.out.print("     Inner loop count: ");
+			
+			int t = 0;
+			while ( t < 0 ) {
+				if (t== 10) break; // terminate loop if t is 10
+				System.out.print(t + " ");
+				
+				t++;	
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+		System.out.println("Loops complete.");
+
+	}
+
+}
