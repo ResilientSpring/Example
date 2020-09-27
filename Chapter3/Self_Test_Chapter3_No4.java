75b59f38f41120fa5f9dd325ee4b3400b3132146
ResilientSpring <ReamsAggravate0615>
2020-09-27 21:44:17 +0800

    Committed 2020/9/27

---
+++ Chapter3/Self_Test_Chapter3_No4.java
@@ -0,0 +1,17 @@
+/*
+ * Show the for statement for a loop that counts from 1000  to 0 by -2. 
+ * */
+public class Self_Test_Chapter3_No4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 1000 ; i > (-1) ; i = i-2) {
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
