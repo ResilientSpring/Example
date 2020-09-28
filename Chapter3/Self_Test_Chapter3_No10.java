b10e51859a2c96d779f1f335bcc325831fc57c0f
ResilientSpring <ReamsAggravate0615>
2020-09-29 00:23:58 +0800

    Committed 2020/09/28

---
+++ Chapter3/Self_Test_Chapter3_No10.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+/*
+ * To convert a lowercase letter to uppercase, subtract 32 from it.
+ * 
+ * */
+public class Self_Test_Chapter3_No10 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		char ch;
+		
+		ch = (char) System.in.read();
+
+	}
+
+}
