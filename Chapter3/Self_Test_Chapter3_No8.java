9bbae66d3b359c0a0d50e79557d03b46a922f8fd
ted10014 <ted10014@gmail.com>
2020-09-29 22:53:12 +0800

    Committed 2020/09/29

---
+++ Chapter3/Self_Test_Chapter3_No8.java
@@ -0,0 +1,18 @@
+
+public class Self_Test_Chapter3_No8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 10; i++) {
+
+			System.out.print(i + " ");
+			if ((i % 2) == 0)
+				continue;
+			System.out.println();
+
+		}
+
+	}
+
+}
---
+++ Chapter3/Self_Test_Chapter3_No9-ted10014-PC.java
@@ -0,0 +1,9 @@
+
+public class Self_Test_Chapter3_No9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
