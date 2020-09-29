0a038f968d8b43fcf6649c260dde368204b0d466
ResilientSpring <ReamsAggravate0615>
2020-09-29 10:58:00 +0800

    Committed 2020/9/29

---
+++ Chapter4/VehicleDemo2.java
@@ -0,0 +1,19 @@
+
+// A program that uses the Vehicle class
+public class Vehicle {
+	
+	int passengers; // number of passengers.
+	int fuelcap;    // fuel capacity in gallons.
+	int mpg;        // fuel consumption in miles per gallon.
+
+}
+
+
+public class VehicleDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter4/VehicleDemo3.java
@@ -0,0 +1,17 @@
+// A program that uses the Vehicle class 
+class Vehicle {
+	
+	int passengers; // number of passengers.
+	int fuelcap;    // fuel capacity in gallons.
+	int mpg;        // fuel consumption in miles per gallon.
+
+}
+
+public class VehicleDemo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
