0b3622c9621d2e021835c965c8104977d13a64fe
ted10014 <ted10014@gmail.com>
2020-10-11 13:26:09 +0800

    Committed 2020/10/11

---
+++ Chapter5/MinMax2.java
@@ -0,0 +1,22 @@
+// Use array initializers. 
+public class MinMax2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nums[] = { 99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};  // Array initializers.
+		
+		int min, max;
+		
+		min = max = nums[0];
+		for (int i = 1; i < 10; i++) {
+			if ( nums[i] < min ) min = nums[i];
+			if ( nums[i] > max ) max = nums[i];
+			
+		}
+		
+		System.out.println("Min and max: " + min + ", " + max);
+
+	}
+
+}
