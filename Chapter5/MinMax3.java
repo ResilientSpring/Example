c41a6a0690a8add296fc41edadbc0cc037db53b8
ted10014 <ted10014@gmail.com>
2020-10-12 14:56:23 +0800

    Committed 2020/10/12

---
+++ Chapter5/MinMax3.java
@@ -0,0 +1,16 @@
+
+public class MinMax3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nums[];
+		nums = new int[10];
+		
+		for (int i = 0; i < 9; i++) {
+			System.out.println("Original values in the created array are: " + nums[i]);
+		}
+
+	}
+
+}
---
+++ Chapter5/Try_this_5_1.java
@@ -0,0 +1,16 @@
+/*
+ * Try This 5-1
+ * 
+ * Demonstrate the bubble sort. 
+ * 
+ * */
+public class Try_this_5_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nums[] = {99, -10, 100123, 18, -987, 5623, 463, -9, 287, 49};
+
+	}
+
+}
