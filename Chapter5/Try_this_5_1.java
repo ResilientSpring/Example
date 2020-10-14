4fa46f74f7d60869a165d340b00ec988369772c1
ResilientSpring <ReamsAggravate0615>
2020-10-14 09:10:58 +0800

    Committed 2020/10/14 to make up the schedule for 2020/10/13

--- Chapter5/Try_this_5_1.java
+++ Chapter5/Try_this_5_1.java
@@ -9,7 +9,39 @@ public class Try_this_5_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int nums[] = {99, -10, 100123, 18, -987, 5623, 463, -9, 287, 49};
+		int nums[] = {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};
+		
+		int a, b, t, size;
+		
+		size = 10; // number of elements.
+		
+		// Display original array 
+		System.out.print("Original array is: ");
+		for (int i = 0; i < size; i++) System.out.print(" " + nums[i]);
+		System.out.println();
+		
+		// This is the Bubble sort. 
+		for(a = 1; a < size; a++) 
+			for (b = size-1; b >= a; b--) {
+				
+				if ( nums[b-1] > nums[b] ) {  // if out of order
+					// exchange elements
+					
+					t = nums[b-1];
+					nums[b-1] = nums[b];
+					nums[b] = t;
+				}
+				
+			}
+		
+		// display sorted array.
+		System.out.print("Sorted array is: ");
+		
+		for (int i = 0; i < size; i++) {
+			System.out.print(" " + nums[i]);
+		}
+		
+		System.out.println();
 
 	}
 
