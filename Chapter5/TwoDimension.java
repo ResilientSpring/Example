ae44bb4f908dc30fde8654a3c2f0f806b819194c
ted10014 <ted10014@gmail.com>
2020-11-16 22:25:37 +0800

    Committed 2020/11/16

---
+++ Chapter5/TwoDimension.java
@@ -0,0 +1,16 @@
+
+public class TwoDimension {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] table = { {1,2,3,4}, {5,6,7,8}, {9,10,11,12} };
+		
+		System.out.println("table[0][0] is " + table[0][0]);
+		System.out.println("table[0][1] is " + table[0][1]);
+		System.out.println("table[0][2] is " + table[0][2]);
+		System.out.println("table[2][3] is " + table[2][3]);
+
+	}
+
+}
