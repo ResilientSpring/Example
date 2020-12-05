1e13b98f589cc8fe0a3e71e3b0ad19623fa8c99d
ted10014 <ted10014@gmail.com>
2020-12-05 21:58:42 +0800

    Committed 2020/12/5

---
+++ Chapter5/StringDemo.java
@@ -0,0 +1,19 @@
+// Introduce String
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// Declare strings in various ways 
+		
+		String str1 = new String("Java strings are objects.");
+		String str2 = "They are constructed various ways. ";
+		String str3 = new String(str2);
+		String str4 = new String(str1);
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		System.out.println(str4);
+
+	}
+
+}
