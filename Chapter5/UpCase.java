941d3dfa5b3b59e5e00f8744e305d4a14fd1fb36
ted10014 <48134466+ResilientSpring@users.noreply.github.com>
2021-01-10 13:21:20 +0800

    Committed 2021/1/9

--- Chapter5/UpCase.java
+++ Chapter5/UpCase.java
@@ -14,6 +14,20 @@ public class UpCase {
 			// This statement turns off the 6th bit. 
 			ch = (char) ( ch & 65503 );  // ch is now uppercase. 
 			
+			/** Binary	    Oct	 Dec   Hex   Glyph
+			 *  100 0001	101	 65	   41	   A
+			 *  110 0001    141  97    61      a
+			 * 
+			 * https://en.wikipedia.org/wiki/ASCII#Printable_characters
+			 * 
+			 * 
+			 * 0000 0000 0110 0001
+			 * 1111 1111 1101 1111  & 
+			 * -----------------------
+			 * 0000 0000 0100 0001
+			 *  
+			 * */
+			
 			System.out.print(ch + " ");
 		}
 
