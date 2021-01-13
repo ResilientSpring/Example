cd7cf9457d33338084e87bed30e38f70d7312c80
ted10014 <48134466+ResilientSpring@users.noreply.github.com>
2021-01-13 15:49:47 +0800

    Committed 2021/1/12

--- Chapter5/LowCase.java
+++ Chapter5/LowCase.java
@@ -13,6 +13,20 @@ public class LowCase {
 			// This statement turns on the 6th bit. 
 			ch = (char) (ch | 32);  // ch is now lowercase. 
 			
+			/** Binary	       Octal-decimal	   Decimal     Hexadecimal     Glyph
+			 *  100 0001	   101	               65           41	              A
+			 *  110 0001       141                 97           61                a
+			 * 
+			 * https://en.wikipedia.org/wiki/ASCII#Printable_characters
+			 * 
+			 * 
+			 * 0000 0000 0100 0001
+			 * 0000 0000 0010 0000  or 
+			 * -----------------------
+			 * 0000 0000 0110 0001
+			 *  
+			 * */
+			
 			System.out.print(ch + " ");
 		}
 
