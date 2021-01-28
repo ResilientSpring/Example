1a4c1f8adc82c54e6f7523ad81bab960a8378ab6
Ceiling_roof <ted10014@hotmail.com>
2021-01-28 13:54:52 +0800

    Committed 2021/1/28

---
+++ Chapter6/SDemo2.java
@@ -0,0 +1,27 @@
+// Use a static method. 
+class StaticMeth{
+	static int val = 1024;  // a static variable
+	
+	// a static method
+	static int valDiv2() {
+		return val/2;
+	}
+	
+}
+
+
+public class SDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("val is " + StaticMeth.val);
+		System.out.println("StaticMeth.valDiv2(): " + StaticMeth.valDiv2());
+		
+		StaticMeth.val = 4;
+		System.out.println("val is " + StaticMeth.val);
+		System.out.println("StaticMeth.valDiv2(): " + StaticMeth.valDiv2());
+
+	}
+
+}
