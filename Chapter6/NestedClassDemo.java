ecf8078dade7bc2053d6f7a16c2d00db1995ab14
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-01-29 17:42:40 +0800

    Committed 2021/1/29

---
+++ Chapter6/NestedClassDemo.java
@@ -0,0 +1,13 @@
+
+public class NestedClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x[] = {3, 2, 1, 5, 6, 9, 7, 8};
+		Outer outOb = new Outer(x);
+		
+		outOb.analyze();
+	}
+
+}
