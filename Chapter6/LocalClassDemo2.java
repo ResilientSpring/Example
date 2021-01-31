a00cd2312b14a96dbe0cb34c04fa5c91d582baf9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-01-31 22:45:09 +0800

    Committed 2021/1/31

---
+++ Chapter6/LocalClassDemo2.java
@@ -0,0 +1,12 @@
+
+public class LocalClassDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocalClassDemo objectClass = new LocalClassDemo();
+		objectClass.main(null).;
+
+	}
+
+}
