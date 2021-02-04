616d3e536c5e157689aeab888408bf51f086b561
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-02-04 17:21:36 +0800

    Committed 2021/2/4

---
+++ Chapter7/Shapes.java
@@ -0,0 +1,14 @@
+
+public class Shapes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Triangle t1 = new Triangle();
+		Triangle t2 = new Triangle();
+		
+		
+
+	}
+
+}
