5c50b2c1ba889f3ef23561d251220f8007a2219d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-02-10 13:42:31 +0800

    Committed 2021/2/10

---
+++ Chapter7/Triangle5.java
@@ -0,0 +1,21 @@
+// A subclass of TwoDshape for Triangles. 
+public class Triangle5 extends TwoDShape5 {
+	
+	private String styleString;
+
+	public Triangle5(String s,double w, double h) {
+		super(w, h);   // Use super() to build the TwoDShape5 constructor. 
+		
+		
+		styleString = s;
+	}
+	
+	double area() {
+		return getWidth() * getHeight() / 2;
+	}
+	
+	void showStyle() {
+		System.out.println("Triangle is " + styleString);
+	}
+
+}
