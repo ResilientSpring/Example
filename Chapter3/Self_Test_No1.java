f237242d9aacbfddaee167fd952d31e349855654
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-02-20 01:46:44 +0800

    Committed 2021/2/19

---
+++ Chapter3/Self_Test_No1.java
@@ -0,0 +1,15 @@
+/*
+ * Write a program that reads characters from the keyboard until a period is received. 
+ * 
+ * Have the program count the number of spaces. Report the total at the end of the program. 
+ * 
+ * */
+public class Self_Test_No1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("The program will read characters ");
+
+	}
+
+}
