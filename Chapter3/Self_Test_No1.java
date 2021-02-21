0a1f648bd209dbe966d0ee7ecf25ed0192be80e8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-02-21 16:29:41 +0800

    Committed 2021/2/21

--- Chapter3/Self_Test_No1.java
+++ Chapter3/Self_Test_No1.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /*
  * Write a program that reads characters from the keyboard until a period is received. 
  * 
@@ -6,10 +8,28 @@
  * */
 public class Self_Test_No1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		System.out.println("The program will read characters ");
-
+		int number_of_space = 0;
+		
+		System.out.println("The program will read characters until a period is received. \nPlease input: ");
+		
+		byte[] b = new byte[50];
+		
+		System.in.read(b);
+		
+		System.out.println("Your input were: ");
+		
+		for (int i = 0; i < b.length; i++) {
+			System.out.print( (char) b[i]);
+			
+			if(b[i] == 32) number_of_space++;
+			
+			if(b[i] == 46) break;
+		}
+		
+		System.out.println();
+		
+		System.out.println("The number of space is: " + number_of_space);
 	}
-
 }
