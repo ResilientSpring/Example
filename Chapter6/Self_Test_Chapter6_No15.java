eb5109da8db07155153e71d897af641bcd79ac46
Ceiling_roof <ted10014@hotmail.com>
2021-03-07 18:54:52 +0800

    Committed 2021/3/7

---
+++ Chapter6/Self_Test_Chapter6_No15.java
@@ -0,0 +1,19 @@
+
+public class Self_Test_Chapter6_No15 {
+	
+	static void vaTest(int ... v) {
+		
+	}
+	
+	static void vaTest(int w, int ... v) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		vaTest(1);
+
+	}
+
+}
