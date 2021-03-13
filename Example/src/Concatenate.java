c9f4d6cc4c4468d7c42e3143abb1e379cf61da32
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-13 17:35:47 +0800

    Committed 2021/3/13

---
+++ Example/src/Concatenate.java
@@ -0,0 +1,15 @@
+
+public class Concatenate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int my_num  = 1 + 2 + 3 + 4 + 5;
+		int my_num2 = 1 + 2 + 3 + 4 + 5;
+		
+		System.out.println(my_num + ", " + my_num2);
+		System.out.println(my_num + my_num2);
+
+	}
+
+}
