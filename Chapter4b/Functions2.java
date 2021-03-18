e3bbb30c770725d05da10e778adf9732a93abe56
Ceiling_roof <ted10014@hotmail.com>
2021-03-19 03:20:52 +0800

    Committed 2021/3/19

---
+++ Chapter4b/Functions2.java
@@ -0,0 +1,15 @@
+
+public class Functions2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		myfuncparam("Paramter 1");
+
+	}
+	
+	static void myfuncparam (String num) {
+		System.out.println("This is a function with parameter value: " + num);
+	}
+
+}
