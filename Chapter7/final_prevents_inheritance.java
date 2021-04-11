0b7a55c023328936a635ee686eb477b747abf7a5
Ceiling_roof <ted10014@hotmail.com>
2021-04-11 12:42:42 +0800

    Committed 2021/4/11

---
+++ Chapter7/final_prevents_inheritance.java
@@ -0,0 +1,16 @@
+final class Analogous {
+	
+}
+
+class backward_looking extends Analogous {  // Error! Can't subclass Analogous
+	
+}
+
+public class final_prevents_inheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
