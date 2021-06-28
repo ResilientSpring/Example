62e17083c17ea1175f07190f707eed31e13b708b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-28 16:38:25 +0800

    Committed 2021/06/28

--- Chapter7/Circle.java
+++ Chapter7/Circle.java
@@ -1,9 +1,13 @@
 
-public class Circle {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Circle extends TwoDShape11 {
+	
+	public Circle() {
+		super();
+	}
+	
+	double area(double radius) {
+		
+		return radius * radius * 3.14;
 	}
 
 }
