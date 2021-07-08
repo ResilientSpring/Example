2a563ab8fd281d61398c3747cd65fc5fca1c0765
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-08 16:15:53 +0800

    Committed 2021/7/8

---
+++ Chapter7/TwoDShape_.java
@@ -0,0 +1,16 @@
+// Even though TwoDShape is a superclass, it is also a completely independent, stand-alone class.
+public class TwoDShape_ extends TwoDShape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TwoDShape shape = new TwoDShape();
+		
+		shape.height = 20;
+		shape.width = 10;
+		
+		shape.showDim();
+
+	}
+
+}
