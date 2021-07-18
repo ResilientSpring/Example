d9de21231a61b2fcfdd798712cd92a85aa775fa4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-18 16:40:56 +0800

    Committed 2021/7/18

---
+++ Chapter7/Polymorphism.java
@@ -0,0 +1,32 @@
+
+class Cohesion{
+	
+	int a, b, c;
+	
+	public Cohesion(int i, int j, int k) {
+		// TODO Auto-generated constructor stub
+		
+		a = i;
+		b = j;
+		c = k;
+	}
+	
+	void polymorphy() {
+		
+		if ( (b * c) > 0) {
+			System.out.println();
+		}	
+		
+	}
+	
+}
+
+
+public class Polymorphism {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
