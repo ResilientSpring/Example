71f25db2cb6a798d7a243a43b0d010c883f74dee
Ceiling_roof <ted10014@hotmail.com>
2021-07-25 16:46:34 +0800

    Committed 2021/7/25

--- Chapter8/bookpack/BookDemo.java
+++ Chapter8/bookpack/BookDemo.java
@@ -28,6 +28,16 @@ public class BookDemo {  // BookDemo is also part of bookpack.
 		// TODO Auto-generated method stub
 		
 		Book booksBook[] = new Book[5];
+		
+		booksBook[0] = new Book("Java: A Beginner's Guide", "Schildt", 2019);
+		booksBook[1] = new Book("Java: The Complete Reference", "Schildt", 2019);
+		booksBook[2] = new Book("Introducing JavaFX 8 Programming", "Schildt", 2015);
+		booksBook[3] = new Book("Red Storm Rising", "Clancy", 1986);
+		booksBook[4] = new Book("On the Road", "Kerouac", 1955);
+		
+		for (int i = 0; i < booksBook.length; i++) {
+			booksBook[i].show();
+		}
 
 	}
 
