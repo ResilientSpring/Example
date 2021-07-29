0633815f08be957cdf7408d62de49b1d5e6fa504
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-30 01:06:23 +0800

    Committed 2021/7/29

--- Chapter8/Methods_in_an_interface_are_implicitly_public.java
+++ Chapter8/Methods_in_an_interface_are_implicitly_public.java
@@ -1,6 +1,11 @@
 
 class frown {
 	
+	private int getNext() {
+		System.out.println("getNext() ought to be overridden.");
+		return 1;
+	}
+	
 }
 
 public class Methods_in_an_interface_are_implicitly_public {
