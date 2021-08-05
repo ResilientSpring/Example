83972b3bb1a68ad4d845a652f25c0d8a346f5fdd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-05 15:09:50 +0800

    Committed 2021/8/5

---
+++ Miscellaneous/Different_package/Final_.java
@@ -0,0 +1,30 @@
+package Different_package;
+
+//Return a String object. 
+class ErrorMsg_final {
+	// Declare final constants 
+	// Error codes. 
+	int outERR = 0;
+	int inERR = 1;
+	int diskERR = 2;
+	int indexERR = 3;  
+	
+	String msgs[] = {
+			"Output Error", 
+			"Input Error",
+			"Disk Full",
+			"Index Out-Of_Bounds"
+	};
+	
+	// Return the error message.
+	String getErrorMsg(int i) {
+		if (i >= 0 & i < msgs.length)
+			return msgs[i];
+		else
+			return "Invalid Error Code";	
+		}
+}
+
+public class Final_ {
+
+}
