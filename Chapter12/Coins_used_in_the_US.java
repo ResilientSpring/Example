8c5a785a8757006aec7ed9e4fb3e24dfff9b1711
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-06 11:46:51 +0800

    Committed 2021/08/06

---
+++ Chapter12/Coins_used_in_the_US.java
@@ -0,0 +1,5 @@
+// An enumeration is a list of named constants that define a new data type.
+public enum Coins_used_in_the_US {
+	penny, nickel, dime, quater, half_dollar, dollar
+
+}
---
+++ Chapter12/Months_in_a_year_Western_calender.java
@@ -0,0 +1,4 @@
+// An enumeration is a list of named constants that define a new data type.
+public enum Months_in_a_year_Western_calender {
+	January, February, March, April, May, June, July, August, September, October, November, December
+}
