9eb317fa0cd8563c8a689204895b817a1e45ea61
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-06 18:32:22 +0800

    Committed 2021/08/06

--- Chapter12/EnumDemo.java
+++ Chapter12/EnumDemo.java
@@ -3,10 +3,40 @@ public class EnumDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Transport tpTransport;
-		
+
 		tpTransport = Transport.AIRPLANE;
+
+		// Compare two enum values.
+		if (tpTransport == Transport.TRAIN) 
+			System.out.println("tp contains TRAIN.\n");
+
+		switch (tpTransport) {
+
+		case CAR: 
+			System.out.println("A car carries people.");
+			break;
+
+		case TRUCK:
+			System.out.println("A truck carries freight.");
+			break;
+
+		case AIRPLANE:
+			System.out.println("An airplane flies.");
+			break;
+
+		case TRAIN:
+			System.out.println("A train runs on rails.");
+			break;
+
+		case BOAT:
+			System.out.println("A boat sails on water.");
+			break;
+
+		default:
+			throw new IllegalArgumentException("Unexpected value: ");
+		}
 
 	}
 
