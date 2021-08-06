498a9f688420a97a4ec3f59a6e702dd823c34153
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-06 17:33:54 +0800

    Committed 2021/08/06

--- Chapter7/Final/Named_constants.java
+++ Chapter7/Final/Named_constants.java
@@ -2,11 +2,14 @@ package Final;
 
 public class Named_constants {
 	
-	int constant = 1;
-	final 
+	static int constant = 1;
+	final static int named_constant = 2;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println(constant);
+		System.out.println(named_constant);
 
 	}
 
