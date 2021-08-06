e69594e7d6dcaf4b80dfb40aab945315d4b8cb26
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-06 17:49:48 +0800

    Committed 2021/08/06

---
+++ Chapter7/Final/Named_constants_.java
@@ -0,0 +1,20 @@
+package Final;
+
+// An enumeration is a list of named constants that define a new data type.
+public enum Named_constants_ {
+	Zero,
+	One,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine
+
+}
+
+class exploitation {
+	Named_constants_ falloutConstants_ = Named_constants_.Eight;
+}
