248a77148fa5bd8ace76c16fcbdb31469780a91e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-06 12:00:08 +0800

    Committed 2021/08/06

---
+++ Oracle_Java_Tutorials/Inheritance/Writing_final_classes_and_methods.java
@@ -0,0 +1,20 @@
+/**Source: https://docs.oracle.com/javase/tutorial/java/IandI/final.html
+ * 
+ */
+package Inheritance;
+
+
+public class Writing_final_classes_and_methods {
+
+
+	public Writing_final_classes_and_methods() {
+		
+	}
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
