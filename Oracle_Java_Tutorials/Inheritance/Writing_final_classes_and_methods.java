df4f55575dcb49477b9fefe8bfb12d1733f77ec4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-06 12:18:23 +0800

    Committed 2021/08/06

--- Oracle_Java_Tutorials/Inheritance/Writing_final_classes_and_methods.java
+++ Oracle_Java_Tutorials/Inheritance/Writing_final_classes_and_methods.java
@@ -1,8 +1,24 @@
 /**Source: https://docs.oracle.com/javase/tutorial/java/IandI/final.html
  * 
+ * Use the final keyword in a method declaration to indicate that the method cannot be overridden by subclasses.
+ * 
+ * You might wish to make a method final if it has an implementation that should not be changed 
+ * and it is critical to the consistent state of the object. 
  */
 package Inheritance;
 
+class ChessAlgorithm {
+	
+	// An enumeration is a list of named constants that define a new data type. 
+	enum ChessPlayer {
+		White,
+		Black
+	}
+	
+	ChessPlayer getFirstPlayer() {
+		return ChessPlayer.White;
+	}
+}
 
 public class Writing_final_classes_and_methods {
 
