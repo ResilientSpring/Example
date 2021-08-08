f6eafdb78d6308c33499f282689c687571ab56fa
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-08 12:28:19 +0800

    Committed 2021/08/08

---
+++ Miscellaneous/Passing_an_array2.java
@@ -0,0 +1,28 @@
+
+public class Passing_an_array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int t[] = new int[10];
+		
+		for (int i = 0; i < t.length; i++) 
+			t[i] = i;
+		
+		for (int i : t) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println("\n===================================================");
+		
+		display(t);
+
+	}
+	
+	static void display(int complex[]) {
+		for (int i = 0; i < complex.length; i++) {
+			System.out.print(complex[i] + " ");
+		}
+	}
+
+}
