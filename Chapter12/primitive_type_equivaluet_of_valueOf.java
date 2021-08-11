667b3bf0201b619c4bf0dbc93d7f83b7c8864b8c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-11 14:21:10 +0800

    Committed 2021/08/11

---
+++ Chapter12/primitive_type_equivaluet_of_valueOf.java
@@ -0,0 +1,13 @@
+
+public class primitive_type_equivaluet_of_valueOf {
+
+	public static void main(String[] args) {
+		
+		// System can directly print out either numeric constants or named constants.
+		System.out.println(200);
+		System.out.println(Transport.valueOf("BOAT"));
+		System.out.println(Transport.TRUCK);
+
+	}
+
+}
