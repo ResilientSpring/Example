a1beb891afbbd8dfea69c6239058302ce9ca13b3
Ceiling_roof <ted10014@hotmail.com>
2021-08-23 02:08:52 +0800

    Committed 2021/8/23

---
+++ Chapter12/EnumDemo3.java
@@ -0,0 +1,14 @@
+// Use an enumeration constructor, instance variable, and method.
+enum Transport{
+	CAR, TRUCK, AIRPLANE, TRAIN, BOAT;
+}
+
+
+public class EnumDemo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
