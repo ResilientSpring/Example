313e81d355d845d3afaf44bd2a8abd25ad2e03cb
Ceiling_roof <ted10014@hotmail.com>
2021-08-23 11:50:55 +0800

    Committed 2021/08/11

---
+++ Chapter12/EnumDemp3.java
@@ -0,0 +1,19 @@
+// All enumerations automatically have two pre-defined methods: values() and valueOf()
+// values() returns an array that contains a list of the enumeration constants.
+public class EnumDemp3 {
+
+	public static void main(String[] args) {
+		Transport tpTransport;
+		
+		System.out.println("Here are all Transport constants: ");
+		
+		// Use values()
+		Transport allTransport[] = Transport.values();  // Obtain an array of Transport constants.
+		
+		for (Transport transport : allTransport) {
+			System.out.println(transport);
+		}
+
+	}
+
+}
