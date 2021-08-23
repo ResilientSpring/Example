3a78387d6a18bdd0d09e920e1679399efd20b827
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-23 13:43:32 +0800

    Committed 2021/08/23

--- Chapter12/Experimental/EnumDemo3_2.java
+++ Chapter12/Experimental/EnumDemo3_2.java
@@ -1,14 +1,32 @@
 package Experimental;
 
 enum Transportation{
+	CAR(65), TRUCK(55), AIRPLANE(600), TRAIN(70), BOAT(22);
 	
+	int typical_speed;
+
+	Transportation(int i) {
+		typical_speed = i;
+	}
+	
+	void show() {
+		System.out.println("Typical speed is at " + typical_speed);
+	}
 }
 
 public class EnumDemo3_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Similar to int a = 1; 
+		Transportation a = Transportation.TRUCK;
+		
+		// Transportation's method.
+		System.out.println(a.typical_speed);
+		
+		a.show();
+		
+		System.out.println("Typical speed of boat is at " + Transportation.BOAT.typical_speed);
 	}
 
 }
