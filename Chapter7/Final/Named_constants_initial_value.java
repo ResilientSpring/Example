464b2b3f22cd256f8f758ea17d145e61b2e8cf14
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-30 13:32:35 +0800

    Committed 2021/08/30

---
+++ Chapter7/Final/Named_constants_initial_value.java
@@ -0,0 +1,20 @@
+package Final;
+
+public class Named_constants_initial_value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final int defuse = 8;
+		
+		defuse = 10;
+		
+		final int bells_and_whistles;
+		
+		bells_and_whistles = 22;
+		
+		bells_and_whistles = 33;
+
+	}
+
+}
