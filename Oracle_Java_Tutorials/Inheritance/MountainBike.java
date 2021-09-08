4cfdccc9167ee1a1ac768665e7e29e1de9fc7c0f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-08 19:19:27 +0800

    Committed 2021/09/08

--- Oracle_Java_Tutorials/Inheritance/MountainBike.java
+++ Oracle_Java_Tutorials/Inheritance/MountainBike.java
@@ -4,6 +4,10 @@ public class MountainBike extends Bicycle {
 	
     // the MountainBike subclass adds one field
     public int seatHeight;
+    
+    // add a field for suspension, which is a String value that indicates if the bike has a front shock absorber, "Front".
+    //  Or, the bike has a front and back shock absorber, "Dual".
+    private String suspension;
 
 	public MountainBike(int startCadence, int startSpeed, int startGear, int startHeight) {
 		super(startCadence, startSpeed, startGear);
@@ -14,7 +18,24 @@ public class MountainBike extends Bicycle {
     // the MountainBike subclass adds one method
     public void setHeight(int newValue) {
         seatHeight = newValue;
-    }   
+    }
+    
+
+    public String getSuspension(){
+      return this.suspension;
+    }
+
+    public void setSuspension(String suspensionType) {
+        this.suspension = suspensionType;
+    }
+
+    // Note the overridden printDescription method. In addition to the information provided before, 
+    // additional data about the suspension is included to the output.
+    public void printDescription() {
+        super.printDescription();
+        System.out.println("The " + "MountainBike has a" +
+            getSuspension() + " suspension.");
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ Oracle_Java_Tutorials/Inheritance/RoadBike.java
@@ -0,0 +1,35 @@
+/* Creating the RoadBike class. Because road or racing bikes have skinny tires, 
+ * add an attribute to track the tire width. Here is the RoadBike class:
+ * */
+
+package Inheritance;
+
+public class RoadBike extends Bicycle {
+
+	// In millimeters (mm)
+	private int tireWidth;
+
+	public RoadBike(int startCadence, int startSpeed, int startGear, int newTireWidth) {
+		super(startCadence, startSpeed, startGear);
+
+	}
+
+	public int getTireWidth() {
+		return this.tireWidth;
+	}
+
+	public void setTireWidth(int newTireWidth) {
+		this.tireWidth = newTireWidth;
+	}
+
+	public void printDescription() {
+		super.printDescription();
+		System.out.println("The RoadBike" + " has " + getTireWidth() + " MM tires.");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
