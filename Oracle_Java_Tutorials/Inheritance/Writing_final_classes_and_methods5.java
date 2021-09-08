475d7c684b5806389e4e401dabe22bc055fd1213
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-09 01:04:03 +0800

    Committed 2021/09/09

---
+++ Oracle_Java_Tutorials/Inheritance/Writing_final_classes_and_methods5.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+class ChessAlgorithm_{
+	enum ChessPlayer{
+		BLACK, WHITE
+	}
+}
+
+public class Writing_final_classes_and_methods5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
