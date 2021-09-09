70536e9db85a5ed0d559a24cea5861e65abb8700
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-10 01:06:13 +0800

    Committed 2021/09/09

---
+++ Oracle_Java_Tutorials/Passing_Information_to_a_Method_or_a_Constructor/Polygon.java
@@ -0,0 +1,21 @@
+package Passing_Information_to_a_Method_or_a_Constructor;
+
+class Point{
+	double x;
+	double y;
+	
+	
+}
+
+public class Polygon {
+	
+	public Polygon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
