f579a116c449838b753d785ae68fdec554742fa2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-09 13:36:54 +0800

    Committed 2021/09/09

---
+++ Oracle_Java_Tutorials/Abstract_Methods_and_Classes/Abstract_classes.java
@@ -0,0 +1,13 @@
+package Abstract_Methods_and_Classes;
+
+public class Abstract_classes {
+	
+	// An abstract class is a class that is declared "abstract"—it may or may not include abstract methods.
+	
+	// Abstract classes cannot be instantiated, but they can be subclassed.
+	
+	// When an abstract class is subclassed, 
+	// the subclass usually provides implementations for all of the abstract methods in its parent class. 
+	// However, if it does not, then the subclass must also be declared abstract.
+
+}
