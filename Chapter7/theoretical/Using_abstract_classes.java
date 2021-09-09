0c80898b84b49696b29ac5a32e54cf5ab0d6f6d1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-09 15:58:52 +0800

    Committed 2021/09/09

---
+++ Chapter7/theoretical/Using_abstract_classes.java
@@ -0,0 +1,18 @@
+package theoretical;
+
+class Using_abstract_classes{
+	private double width;
+	private double height;
+	private String name;
+	
+	
+}
+
+public class Using_abstract_classes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
