06e1fd7bef0b54885fc39b385a9d0a19c85e77de
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-09 18:06:29 +0800

    Committed 2021/09/09

---
+++ Chapter6/Using_class_type_as_parameters_to_methods/A_variable_of_a_class_type.java
@@ -0,0 +1,10 @@
+package Using_class_type_as_parameters_to_methods;
+
+public class A_variable_of_a_class_type {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
