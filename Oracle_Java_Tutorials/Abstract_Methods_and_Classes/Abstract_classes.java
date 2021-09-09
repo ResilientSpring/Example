0c4299646fcbac19b61b13d661cb4c72e789ab24
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-09 13:48:08 +0800

    Committed 2021/09/09

--- Oracle_Java_Tutorials/Abstract_Methods_and_Classes/Abstract_classes.java
+++ Oracle_Java_Tutorials/Abstract_Methods_and_Classes/Abstract_classes.java
@@ -9,5 +9,8 @@ public class Abstract_classes {
 	// When an abstract class is subclassed, 
 	// the subclass usually provides implementations for all of the abstract methods in its parent class. 
 	// However, if it does not, then the subclass must also be declared abstract.
+	
+	// Reference: 
+	// https://docs.oracle.com/javase/tutorial/java/IandI/abstract.html
 
 }
