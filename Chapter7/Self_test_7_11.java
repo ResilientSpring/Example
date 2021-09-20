8a535e4071450981ba49e71390ce1df38590f6d2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-20 16:58:15 +0800

    Committed 2021/09/20

---
+++ Chapter7/Self_test_7_11.java
@@ -0,0 +1,30 @@
+class A_{
+
+}
+
+class B_ extends A_{
+	
+	public B_() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public A_ make_ob(int which) {
+		// TODO Auto-generated method stub
+		if (which == 0)
+			return new A_();
+		else 
+			return new B_();
+	}
+}
+
+public class Self_test_7_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		B_ aversionB_ = new B_();
+		
+
+	}
+
+}
