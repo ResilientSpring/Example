ff31a95c533ae09a59ea4be7a409e3c87712c3b7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-20 17:30:29 +0800

    Committed 2021/09/20

--- Chapter7/Self_test_7_14.java
+++ Chapter7/Self_test_7_14.java
@@ -1,8 +1,29 @@
 
+class A__{
+	
+}
+
+class B__ extends A__{
+	
+	public A__ makeObj (int which) {
+		if(which == 0)
+			return new A__();
+		else {
+			return new B__();
+		}
+	}
+}
+
 public class Self_test_7_14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		B__ aversionB__ = new B__();
+		
+		var myRef = (B__) aversionB__.makeObj(1);
+		
+		var check = myRef.makeObj(0);
 
 	}
 
