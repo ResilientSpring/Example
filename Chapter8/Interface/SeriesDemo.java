db016dc741c09223abd755f4d164a4a8f090a54a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-21 17:43:59 +0800

    Committed 2021/09/21

--- Chapter8/Interface/SeriesDemo.java
+++ Chapter8/Interface/SeriesDemo.java
@@ -1,6 +1,35 @@
 package Interface;
 
-class ByTwos {
+class ByTwos implements Series {
+	
+	int start;
+	int val;
+	
+	public ByTwos() {
+		// TODO Auto-generated constructor stub
+		start = 0;
+		val = 0;
+	}
+
+	@Override
+	public int getNext() {
+		// TODO Auto-generated method stub
+		val += 2;
+		return val;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		val = start;
+	}
+
+	@Override
+	public void setStart(int x) {
+		// TODO Auto-generated method stub
+		start = x;
+		val = x;
+	}
 	
 }
 
@@ -8,6 +37,24 @@ public class SeriesDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ByTwos obByTwos = new ByTwos();
+		
+		for (int i = 0; i < 5; i++) 
+			System.out.println("Next value is " + obByTwos.getNext());
+		
+		System.out.println("\nResetting");
+		obByTwos.reset();
+		
+		for (int i = 0; i < 5; i++) 
+			System.out.println("Next value is " + obByTwos.getNext());
+		
+		System.out.println("\nStarting at 100");
+		
+		obByTwos.setStart(100);
+		
+		for (int i = 0; i < 5; i++)
+			System.out.println("Next value is " + obByTwos.getNext());
 
 	}
 
