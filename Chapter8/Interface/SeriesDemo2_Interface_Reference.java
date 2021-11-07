ed1337608e0b3f51c68815008c9c4d421d678a3c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-07 15:23:36 +0800

    Committed 2021/11/07

--- Chapter8/Interface/SeriesDemo2_Interface_Reference.java
+++ Chapter8/Interface/SeriesDemo2_Interface_Reference.java
@@ -8,8 +8,7 @@ class ByTwos_instinct implements Series {
 	@Override
 	public int getNext() {
 		val += 2; 
-		
-		return 0;
+		return val;
 	}
 
 	@Override
@@ -35,7 +34,7 @@ class ByThrees_instinct implements Series {
 	@Override
 	public int getNext() {
 		val += 3;
-		return 0;
+		return val;
 	}
 
 	@Override
@@ -57,6 +56,22 @@ public class SeriesDemo2_Interface_Reference {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ByTwos_instinct twos_instinct = new ByTwos_instinct();
+		
+		ByThrees_instinct threes_instinct = new ByThrees_instinct();
+		
+		Series refSeries;
+		
+		for (int i = 0; i < 5; i++) {
+			refSeries = twos_instinct;
+			
+			System.out.println("Next ByTwos value is " + refSeries.getNext());
+			
+			refSeries = threes_instinct;
+			
+			System.out.println("Next ByThrees value is " + refSeries.getNext());
+		}
 
 	}
 
