94340b7f0500b45c4c6c337bfe3e74c8b68ba67e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-07 14:42:53 +0800

    Committed 2021/11/07

---
+++ Chapter8/Interface/SeriesDemo2_Interface_Reference.java
@@ -0,0 +1,32 @@
+package Interface;
+
+class ByTwos_instinct implements Series {
+
+	@Override
+	public int getNext() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStart(int x) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+public class SeriesDemo2_Interface_Reference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
