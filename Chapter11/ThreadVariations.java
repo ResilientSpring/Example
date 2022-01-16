9d44f71725ce0e73ee9606c9ca8e9ddd67df43f2
Ceiling_roof <ted10014@hotmail.com>
2022-01-16 14:04:32 +0800

    Committed 2021/01/16

---
+++ Chapter11/ThreadVariations.java
@@ -0,0 +1,11 @@
+// MyThread variations. This version of MyThread creates a Thread when its constructor is called and 
+// stores it in an instance variable called thread_name_String. 
+// It also sets the name of the thread and provides a factory method to create and start a thread.
+public class ThreadVariations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
