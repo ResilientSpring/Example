041a5fffa09207729488c46cb1799cf782310441
Ceiling_roof <ted10014@hotmail.com>
2022-01-16 14:30:53 +0800

    Committed 2022/01/16

--- Chapter11/ThreadVariations.java
+++ Chapter11/ThreadVariations.java
@@ -9,3 +9,30 @@ public class ThreadVariations {
 	}
 
 }
+
+class MyThread_improvement implements Runnable{
+	
+	Thread thread;  // A reference to the thread is stored in thread.
+	
+	public MyThread_improvement(String name) {
+		thread = new Thread(this, name); // The thread is named when it is created.
+		
+	}
+	
+	// A factory method that creates and starts a thread.
+	public static MyThread_improvement createAndStart(String name) {
+		MyThread_improvement myThread_improvement = new MyThread_improvement(name);
+		
+		myThread_improvement.thread.start();
+		
+		return myThread_improvement;
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+	
+}
