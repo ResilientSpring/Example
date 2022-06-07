c98979c2c53246104e4c1596fb8f71dd1755fe23
Ceiling_roof <ted10014@hotmail.com>
2022-06-08 00:45:21 +0800

    Committed 2022/06/08

---
+++ Miscellaneous/Cpp/Array_of_objects.java
@@ -0,0 +1,20 @@
+package Cpp;
+
+import javax.security.auth.x500.X500Principal;
+
+class MyClass{
+	private int x;
+	
+	void set_x(int i) {
+		x = i;
+	}
+}
+
+public class Array_of_objects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
