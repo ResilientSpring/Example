6433993a13541082542bb2c40dcead67b69034ae
Ceiling_roof <ted10014@hotmail.com>
2022-06-17 13:15:33 +0800

    Committed 2022/06/17

---
+++ Miscellaneous/Cpp/Array_of_strings_3.java
@@ -0,0 +1,34 @@
+package Cpp;
+
+import java.io.IOException;
+
+public class Array_of_strings_3 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String dictionary[][] = { { "Pencil", "A written instrument." }, { "Keyboard", "An input device." },
+				{ "rifle", "A shoulder-fired firearm." }, { "Airplane", "A fixed-wing aircraft." },
+				{ "network", "An interconnected group of computers." } };
+
+		byte[] word = new byte[80];
+
+		System.out.println("Enter word: ");
+		System.in.read(word);
+		System.out.println("You entered: ");
+		for (int i = 0; i < word.length; i++)
+			System.out.print((char) word[i]);
+		
+		String convertionString = String.valueOf(word);
+		
+		System.out.println(convertionString);
+		
+		StringBuilder sBuilder = new StringBuilder();
+		for (byte b : word) 
+			sBuilder.append((char) b);
+		
+		System.out.println(sBuilder);
+		
+		
+	}
+
+}
