b01c23b4f71029479e1ff7a50e993fc4c06c82f7
Ceiling_roof <ted10014@hotmail.com>
2022-06-17 14:02:48 +0800

    Committed 2022/06/17

--- Chapter10/Reading_console_output.java
+++ Chapter10/Reading_console_output.java
@@ -7,9 +7,13 @@ public class Reading_console_output {
 		
 		byte data[] = new byte[10];
 		
-		System.out.println("Enter some characters.");
+		System.out.println("Enter some characters:");
 		System.in.read(data);
-
+		System.out.print("You entered: ");
+		
+		for (int i = 0; i < data.length; i++) 
+			System.out.print((char) data[i]);
+		
 	}
 
 }
