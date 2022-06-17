4cb2f5f0dd930dd6f3ed7bc7a00ea59c04fb4c86
Ceiling_roof <ted10014@hotmail.com>
2022-06-17 11:23:52 +0800

    Committed 2022/06/17

---
+++ Miscellaneous/Cpp/Array_of_strings.java
@@ -0,0 +1,13 @@
+package Cpp;
+
+public class Array_of_strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String dictionary[][] = {{"Pencil", "A written instrument."}, {"Keyboard", "An input device."}, 
+				{"rifle", "A shoulder-fired firearm."}, {"Airplane", "A fixed-wing aircraft."}, 
+				{"network", "An interconnected group of computers."}};
+	}
+
+}
