87a8564534a7de8425c52711eafb6898d71e2004
Ceiling_roof <ted10014@hotmail.com>
2022-06-17 12:12:55 +0800

    Committed 2022/06/17

--- Miscellaneous/Cpp/Array_of_strings.java
+++ Miscellaneous/Cpp/Array_of_strings.java
@@ -6,19 +6,19 @@ public class Array_of_strings {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
-		String dictionary[][] = {{"Pencil", "A written instrument."}, {"Keyboard", "An input device."}, 
-				{"rifle", "A shoulder-fired firearm."}, {"Airplane", "A fixed-wing aircraft."}, 
-				{"network", "An interconnected group of computers."}};
-		
+
+		String dictionary[][] = { { "Pencil", "A written instrument." }, { "Keyboard", "An input device." },
+				{ "rifle", "A shoulder-fired firearm." }, { "Airplane", "A fixed-wing aircraft." },
+				{ "network", "An interconnected group of computers." } };
+
 		byte[] word = new byte[80];
-		
+
 		System.out.println("Enter word: ");
 		System.in.read(word);
 		System.out.println("You entered: ");
-		for (int i = 0; i < word.length; i++) {
-			System.out.println((char) word[i]);
-		}
+		for (int i = 0; i < word.length; i++)
+			System.out.print((char) word[i]);
+
 	}
 
 }
