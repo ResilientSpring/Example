8f7bf3c51fef0bd3915da6ac74094bf26d5b2f65
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-19 13:51:45 +0800

    Committed 2022/06/19

---
+++ Miscellaneous/Cpp/Pointer_to_object.java
@@ -0,0 +1,22 @@
+package Cpp;
+
+class P_example {
+	private int num;
+	
+	void set_num(int val) {
+		num = val;
+	}
+	
+	void show_num() {
+		System.out.println(num);
+	}
+}
+
+public class Pointer_to_object {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
