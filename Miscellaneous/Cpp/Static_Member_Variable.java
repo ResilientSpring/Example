853481072bbb02c6867b0a83bacb91252ae2974b
Ceiling_roof <ted10014@hotmail.com>
2023-01-26 23:36:59 +0800

    Committed 2023/01/26

---
+++ Miscellaneous/Cpp/Static_Member_Variable.java
@@ -0,0 +1,14 @@
+package Cpp;
+
+class ShareVar{
+	static private int num;
+}
+
+public class Static_Member_Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
