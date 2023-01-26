df46be20081bd6c7c5f9e06f1c3b94930f740a82
Ceiling_roof <ted10014@hotmail.com>
2023-01-26 23:42:24 +0800

    Committed 2023/01/26

--- Miscellaneous/Cpp/Static_Member_Variable.java
+++ Miscellaneous/Cpp/Static_Member_Variable.java
@@ -16,6 +16,18 @@ public class Static_Member_Variable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ShareVar aShareVar = new ShareVar();
+		ShareVar bShareVar = new ShareVar();
+		
+		aShareVar.shownum();
+		bShareVar.shownum();
+		
+		aShareVar.setnum(10);
+		
+		aShareVar.shownum();
+		bShareVar.shownum();
+		
 
 	}
 
