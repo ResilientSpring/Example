b798c99a69a13416ab1f59244973f452e6436f7f
Ceiling_roof <ted10014@hotmail.com>
2023-08-13 14:40:00 +0800

    Committed on or around 2023/08/13

---
+++ Miscellaneous/Cpp/static_variable.java
@@ -0,0 +1,15 @@
+package Cpp;
+
+class ShareVar{
+	
+	static int num;
+}
+
+public class static_variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
