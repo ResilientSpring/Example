b364e1ab3c573749857842953ec5ffc3fd60228b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-14 23:49:28 +0800

    Committed on or around 2023/08/14

--- Miscellaneous/many_Binary_Search_Tree/Binary_Search_Trees_non_static_nested_class_2.java
+++ Miscellaneous/many_Binary_Search_Tree/Binary_Search_Trees_non_static_nested_class_2.java
@@ -1,5 +1,13 @@
 package many_Binary_Search_Tree;
 
+class Node{
+	
+	Node leftNode;
+	Node rightNode;
+	int value;
+
+}
+
 public class Binary_Search_Trees_non_static_nested_class_2 {
 
 	public static void main(String[] args) {
