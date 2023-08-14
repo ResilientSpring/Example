d4b3d16f2d44d01dbce130811f16c9ec695ee1ad
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-14 23:47:32 +0800

    Committed on or around 2023/08/14

---
+++ Miscellaneous/many_Binary_Search_Tree/Binary_Search_Trees_non_static_nested_class_2.java
@@ -0,0 +1,10 @@
+package many_Binary_Search_Tree;
+
+public class Binary_Search_Trees_non_static_nested_class_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
