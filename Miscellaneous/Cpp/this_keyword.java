047fe10f8a93ab50d2a62973ff4dd90cc1c3b815
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-16 14:50:43 +0800

    Committed on or around 2023/08/16

---
+++ Miscellaneous/Cpp/this_keyword.java
@@ -0,0 +1,23 @@
+package Cpp;
+
+class Function_that_returns_objects{
+	
+	int value;
+	
+	public Function_that_returns_objects(int value) {
+		
+		this.value = value;
+		
+	}
+}
+
+public class this_keyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
