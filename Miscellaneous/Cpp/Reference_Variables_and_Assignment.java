acb6d709fc56b4a90ef1e732c230c0d713240b1f
Ceiling_roof <ted10014@hotmail.com>
2023-08-19 00:12:59 +0800

    Committed on or around 2023/08/19

--- Miscellaneous/Cpp/Reference_Variables_and_Assignment.java
+++ Miscellaneous/Cpp/Reference_Variables_and_Assignment.java
@@ -1,6 +1,6 @@
 package Cpp;
 
-class car{
+class Car{
 	
 	int passengers;
 	int fuelcap;
@@ -12,6 +12,12 @@ public class Reference_Variables_and_Assignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Car vehicle1 = new Car();
+		Car vehicle2 = vehicle1;
+		
+		vehicle1.mpg = 26;
+		
 
 	}
 
