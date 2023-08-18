f454e818c762c6c0ac3d757da577e6cd02606030
Ceiling_roof <ted10014@hotmail.com>
2023-08-18 22:41:05 +0800

    Committed on or around 2023/08/18

---
+++ Miscellaneous/Cpp/Assigning_Objects.java
@@ -0,0 +1,23 @@
+package Cpp;
+
+class Test{
+	
+	int a, b;
+	
+	void set_ab(int a, int b) {
+		
+		this.a = a;
+		this.b = b;
+		
+	}
+	
+}
+
+public class Assigning_Objects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
