fd3748e3a157141bbbf3a8e3de0ac02f4c2c78d0
Ceiling_roof <ted10014@hotmail.com>
2023-08-19 23:52:40 +0800

    Committed on or around 2023/08/19

---
+++ Miscellaneous/Cpp/Passing_Objects_to_Functions.java
@@ -0,0 +1,15 @@
+package Cpp;
+
+class My_Class{
+	
+	int value;
+}
+
+public class Passing_Objects_to_Functions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
