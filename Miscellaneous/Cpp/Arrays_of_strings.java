2cc9e8c232d58716a2fded9ea30dc036498c1118
Ceiling_roof <ted10014@hotmail.com>
2023-08-27 13:17:11 +0800

    Committed on or around 2023/08/27

---
+++ Miscellaneous/Cpp/Arrays_of_strings.java
@@ -0,0 +1,19 @@
+package Cpp;
+
+public class Arrays_of_strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String numbers[][] = {
+				
+				{"Tom", "555-3322"},
+				{"Mary", "555-8976"},
+				{"Jon", "555-1037"},
+				{"Rachel", "555-1400"},
+				{"Sherry", "555-8873"}
+		};
+
+	}
+
+}
