34756d5c07220c8d9671aa7f05c90ce9035baaa9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-23 21:05:07 +0800

    Committed on or around 2023/09/23

--- Chapter7/theoretical/Using_super_to_call_superclass_constructor.java
+++ Chapter7/theoretical/Using_super_to_call_superclass_constructor.java
@@ -42,12 +42,34 @@ class Triangular extends TwoDShaper{
 		
 		style = s;
 	}
+	
+	double area() {
+		return getWidth() * getHeight() / 2;
+	}
+	
+	void showStyle() {
+		System.out.println("Triangle is " + style);
+	}
 }
 
 public class Using_super_to_call_superclass_constructor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Triangular t1 = new Triangular("filled", 4.0, 4.0);
+		Triangular t2 = new Triangular("outlined", 8.0, 12.0);
+		
+		System.out.println("Info for t1: ");
+		t1.showStyle();
+		t1.showDim();
+		System.out.println("Area is " + t1.area());
+		
+		System.out.println();
+		
+		System.out.println("Info for t2: ");
+		t2.showStyle();
+		t2.showDim();
+		System.out.println("Area is " + t2.area());		
 
 	}
 
