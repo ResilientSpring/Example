c910642d24a97695f13d72e17c0ece2f3f1a719f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-24 00:18:33 +0800

    Committed on or around 2023/09/24

--- Chapter7/theoretical/Using_super_to_call_superclass_constructor_3.java
+++ Chapter7/theoretical/Using_super_to_call_superclass_constructor_3.java
@@ -97,6 +97,30 @@ public class Using_super_to_call_superclass_constructor_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Triangular_3 t1 = new Triangular_3("filled", 4.0, 4.0);
+		Triangular_3 t2 = new Triangular_3("outlined", 8.0, 12.0);
+		Triangular_3 t3 = new Triangular_3("Slovenly", "Classy", 8.0, 9.0);
+		
+		System.out.println("Info for t1: ");
+		t1.showStyle();
+		t1.showDim();
+		System.out.println("Area is " + t1.area());
+		
+		System.out.println();
+		
+		System.out.println("Info for t2: ");
+		t2.showStyle();
+		t2.showDim();
+		System.out.println("Area is " + t2.area());		
+		
+		System.out.println();
+		
+		System.out.println("Info for t3: ");
+		t3.showStyle();
+		t3.showDim();
+		t3.showTitle();
+		t3.showTheTitle();
+		System.out.println("Area is " + t3.area());
 
 	}
 
