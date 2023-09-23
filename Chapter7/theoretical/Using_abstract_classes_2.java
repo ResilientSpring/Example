48093ae1f3b9cad58a00465e48dbdd88331b5965
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-23 12:53:45 +0800

    Committed on or around 2023/09/23

---
+++ Chapter7/theoretical/Using_abstract_classes_2.java
@@ -0,0 +1,26 @@
+package theoretical;
+
+abstract class TwoDShapes{
+	
+	private double width;
+	private double height;
+	private String name;
+	
+	public TwoDShapes() {
+		
+		width = height = 0.0;
+		name = "none";
+	}
+}
+
+
+public class Using_abstract_classes_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
