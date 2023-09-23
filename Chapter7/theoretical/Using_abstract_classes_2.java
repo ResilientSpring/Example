075903c706565e9eef03f526f714228158c28c53
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-23 13:44:28 +0800

    Committed on or around 2023/09/23

--- Chapter7/theoretical/Using_abstract_classes_2.java
+++ Chapter7/theoretical/Using_abstract_classes_2.java
@@ -29,6 +29,16 @@ abstract class TwoDShapes{
 		height = ob.height;
 		name = ob.name;
 	}
+	
+	
+	double getWidth() {
+		return width;
+	}
+	
+	double getHeight() {return height;}
+	void setWidth(double w) { width = w;}
+	void setHeight(double h) {height = h;}
+	String getName() {return name;}
 }
 
 
