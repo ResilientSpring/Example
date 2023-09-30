9d5a44c7217ec0b280314b8faa6496bc22c6722f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-30 17:42:19 +0800

    Committed on or around 2023/09/30

---
+++ Chapter7/theoretical/Create_a_multilevel_hierarchy.java
@@ -0,0 +1,19 @@
+package theoretical;
+
+class TwoDShape_1 {
+	private double width;
+	private double height;
+
+	public TwoDShape_1() {
+		width = height = 0.0;
+	}
+}
+
+public class Create_a_multilevel_hierarchy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
