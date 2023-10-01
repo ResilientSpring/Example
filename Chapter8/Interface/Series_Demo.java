edac15bb2a03547b7f3984ec64a714a64e8fbba2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-01 15:02:16 +0800

    Committed on or around 2023/10/01

--- Chapter8/Interface/Series_Demo.java
+++ Chapter8/Interface/Series_Demo.java
@@ -19,9 +19,15 @@ class ByTwos implements Series2{
 		value = 0;
 	}
 	
+	// Whenever you implement a method defined by an interface, it must be implemented as public; 
+	// because all members of an interface are implicitly public.
 	public int getNext() {
 		value += 2;
 		return value;
+	}
+	
+	public void reset() {
+		value = start;
 	}
 }
 
