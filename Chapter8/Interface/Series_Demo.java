0a4b2f79b079b8212cffb61e247c0e5c20d260bd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-01 14:53:52 +0800

    Committed on or around 2023/10/01

--- Chapter8/Interface/Series_Demo.java
+++ Chapter8/Interface/Series_Demo.java
@@ -8,10 +8,26 @@ interface Series2{
 	void setStart(int x);
 }
 
+
+// Implement Series.
+class ByTwos implements Series2{
+	int start; 
+	int value;
+	
+	public ByTwos() {
+		start = 0;
+		value = 0;
+	}
+	
+	int getNext() {
+		
+	}
+}
+
 public class Series_Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
