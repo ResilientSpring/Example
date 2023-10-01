52fa5931c0ac2f8433f21e0b02522760cb85fc4b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-01 14:04:13 +0800

    Committed on or around 2023/10/01

---
+++ Chapter8/Interface/Series_Demo.java
@@ -0,0 +1,15 @@
+package Interface;
+
+// An interface specifies what must be done, but not how to do it.
+interface Series2{
+	
+}
+
+public class Series_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
