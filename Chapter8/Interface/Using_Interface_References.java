921615945e32ed20de127e50497ae70096dc6649
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-02 22:25:57 +0800

    Committed on or around 2023/10/02

---
+++ Chapter8/Interface/Using_Interface_References.java
@@ -0,0 +1,21 @@
+package Interface;
+
+// An interface specifies what to do, not how to do.
+interface Seriess{
+	int getNext();
+	void reset();
+	void setStart(int x);
+}
+
+class ByTwoss{
+	
+}
+
+public class Using_Interface_References {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
