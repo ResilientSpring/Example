9983e266ba3beb85c7d7aae5eed231ad913aad7d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-03 20:08:53 +0800

    Committed on or around 2023/10/03

---
+++ Chapter8/Try_This_8_1.java
@@ -0,0 +1,19 @@
+// A character queue interface
+interface ICharQ{
+	
+	// Put a character into the queue.
+	void put(char ch);
+	
+	// Get a character from the queue.
+	char get();
+}
+
+
+public class Try_This_8_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
