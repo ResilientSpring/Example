4b36e2baae369bc31a3930fc5e8f53c592d14ba7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-03 20:47:06 +0800

    Committed on or around 2023/10/03

--- Chapter8/Try_This_8_1.java
+++ Chapter8/Try_This_8_1.java
@@ -90,6 +90,18 @@ class CircularQueue implements ICharQ{
 
 
 class DynQueue implements ICharQ{
+
+	@Override
+	public void put(char ch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public char get() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 }
 
