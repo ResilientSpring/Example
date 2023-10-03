c6118f9e52c7b5064a0077be61cd36ba0d7644a5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-03 20:46:52 +0800

    Committed on or around 2023/10/03

--- Chapter8/Try_This_8_1.java
+++ Chapter8/Try_This_8_1.java
@@ -44,16 +44,53 @@ class FixedQueue implements ICharQ{
 class CircularQueue implements ICharQ{
 	private char q[];  // this array holds the queue.
 	private int putloc, getloc;  // the put and get indices.
+	
+	// Construct an empty queue given its size.
+	public CircularQueue(int size) {
+		q = new char[size + 1];  // allocate memory for queue
+		putloc = getloc = 0;
+	}
 
 	public void put(char ch) {
-		// TODO Auto-generated method stub
+		/*
+		 * Queue is full if either putloc is one less than getloc, 
+		 * or if putloc is at the end of the array and getloc is at 
+		 * the beginning.
+		 * */
+		
+		if (putloc + 1 == getloc | ((putloc == q.length - 1) & (getloc == 0))) {
+			System.out.println(" - Queue is full. ");
+			return;
+		}
+		
+		q[putloc++] = ch;
+		
+		if (putloc == q.length) {
+			putloc = 0;
+		}
 		
 	}
 
 	public char get() {
 		// TODO Auto-generated method stub
-		return 0;
+		if (getloc == putloc) {
+			System.out.println(" - Queue is empty.");
+			return (char) 0;
+		}
+		
+		char ch = q[getloc++];
+		
+		if (getloc == q.length) {
+			getloc = 0;
+		}
+		
+		return ch;
 	}
+}
+
+
+class DynQueue implements ICharQ{
+	
 }
 
 
