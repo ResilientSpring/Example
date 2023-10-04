fdd7c3de3831b4b30a87b1ef9f3ef5dcbbde3c05
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-04 14:12:36 +0800

    Committed on or around 2023/10/04

--- Chapter8/Interface/Interfaces_Can_Be_Extended.java
+++ Chapter8/Interface/Interfaces_Can_Be_Extended.java
@@ -1,17 +1,50 @@
 package Interface;
 
+// One interface can extend another.
 interface A{
 	
 	void meth1();
 	void meth2();
 }
 
+// B now includes meth1() and meth2() - it adds meth3().
+interface B extends A{  // B inherits A.
+	void meth3();
+}
+
+// This class must implement all of A and B
+class MyClass implements B{
+
+	@Override
+	public void meth1() {
+		// TODO Auto-generated method stub
+		System.out.println("Implement meth1(). ");
+	}
+
+	@Override
+	public void meth2() {
+		// TODO Auto-generated method stub
+		System.out.println("Implement meth2(). ");
+	}
+
+	@Override
+	public void meth3() {
+		// TODO Auto-generated method stub
+		System.out.println("Implement meth3(). ");
+	}
+}
 
 
 public class Interfaces_Can_Be_Extended {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		MyClass obMyClass = new MyClass();
+		
+		obMyClass.meth1();
+		obMyClass.meth2();
+		obMyClass.meth3();
 
 	}
 
