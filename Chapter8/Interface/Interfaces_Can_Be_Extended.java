cc0f63f4c15dbe5ed2a8bb7410c7623a017bfa7c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-04 14:02:22 +0800

    Committed on or around 2023/10/04

---
+++ Chapter8/Interface/Interfaces_Can_Be_Extended.java
@@ -0,0 +1,18 @@
+package Interface;
+
+interface A{
+	
+	void meth1();
+	void meth2();
+}
+
+
+
+public class Interfaces_Can_Be_Extended {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
