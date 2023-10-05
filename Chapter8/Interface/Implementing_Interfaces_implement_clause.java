18159654cd1f23c0e502750cd816703840c40391
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-05 18:39:04 +0800

    Committed on or around 2023/10/05

---
+++ Chapter8/Interface/Implementing_Interfaces_implement_clause.java
@@ -0,0 +1,57 @@
+package Interface;
+
+// An interface specifies what to do, not how to do.
+interface management{
+	
+	void parenting();
+	void baby_talks();
+	
+	
+}
+
+class parents {
+	
+	String fatherString;
+	String motherString;
+	
+}
+
+
+class kids extends parents implements management{
+
+	@Override
+	public void parenting() {
+		// TODO Auto-generated method stub
+		System.out.println(fatherString + "'s motherhood");
+	}
+
+	@Override
+	public void baby_talks() {
+		// TODO Auto-generated method stub
+		System.out.println( motherString + "'s motherese");
+	}
+	
+}
+
+
+public class Implementing_Interfaces_implement_clause {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		kids janeKids = new kids();
+		kids peterKids = new kids();
+		
+		janeKids.fatherString = "Jansen";
+		janeKids.motherString = "Sharon";
+		janeKids.baby_talks();
+		janeKids.parenting();
+		
+		
+		peterKids.fatherString = "Bill";
+		peterKids.motherString = "Laura";
+		
+
+	}
+
+}
