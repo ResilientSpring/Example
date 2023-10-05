ed8b5f59ab8ece9567e600ba78929df0c9d20250
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-05 19:48:58 +0800

    Committed on or around 2023/10/05

--- Chapter11/Creating_a_Thread.java
+++ Chapter11/Creating_a_Thread.java
@@ -1,4 +1,12 @@
 // Create a thread by implementing Runnable.
+class MyThread2023 implements Runnable{
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
 
 
 public class Creating_a_Thread {
