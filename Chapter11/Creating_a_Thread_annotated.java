b94e61a11a492c0635a23bad583422f6c456b088
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-06 12:49:19 +0800

    Committed on or around 2023/10/06

--- Chapter11/Creating_a_Thread_annotated.java
+++ Chapter11/Creating_a_Thread_annotated.java
@@ -17,7 +17,8 @@ public class Creating_a_Thread_annotated {
 		
 		// To create another thread from within the main thread, construct a MyThread object first.
 		
-		
+		// You create a thread by instantiating an object of type Thread whose constructor encapsulates an object
+		// that is runnable.
 		Thread newtThread = new Thread();
 //		newtThread.setContextClassLoader(myThread2023);
 
