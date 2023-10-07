4df7a99026760221220d277c6a19ead5a3b053a8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-07 14:15:04 +0800

    Committed on or around 2023/10/07

--- Chapter11/One_Improvement_and_Two_Simple_Variations.java
+++ Chapter11/One_Improvement_and_Two_Simple_Variations.java
@@ -1,6 +1,21 @@
 // MyThread variations. This version of MyThread creates a Thread when its constructor is called and stores it in
-// an instance variable called thrd.
+// an instance variable called thread.
 // It also sets the name of thread and provides a factory method to create and start a thread.
+
+class MyThread20231007 implements Runnable{
+	
+	Thread thread;  // A reference to the thread is stored in thread.
+	
+	// Construct a new thread using this Runnable and give it a name.
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
 public class One_Improvement_and_Two_Simple_Variations {
 
 	public static void main(String[] args) {
