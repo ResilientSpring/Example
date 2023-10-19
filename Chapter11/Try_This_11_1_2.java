c499aa4e35fcc321f6be49172615a79c3c26c4b4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-19 14:57:59 +0800

    Committed on or around 2023/10/19

---
+++ Chapter11/Try_This_11_1_2.java
@@ -0,0 +1,62 @@
+
+class ExtendThread2 extends Thread{
+	
+	public ExtendThread2(String name) {
+		super(name);
+	}
+	
+	static ExtendThread2 createAndStart(String name) {
+		
+		ExtendThread2 myExtendThread2 = new ExtendThread2(name);
+		
+		myExtendThread2.start();
+		
+		return myExtendThread2;
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println(getName() + " starting.");
+		
+		try {
+			for (int count = 0; count < 10; count++) {
+				Thread.sleep(400);
+				System.out.println("In " + getName() + ", count is " + count);
+			}
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+			System.out.println(getName() + " interrupted.");
+		}
+		
+		super.run();
+		
+		System.out.println(getName() + " terminating.");
+	}
+	
+}
+
+public class Try_This_11_1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main thread starting.");
+		
+		ExtendThread2 myExtendThread = new ExtendThread2("Child #1");
+		
+		myExtendThread.start();
+		
+		for (int i = 0; i < 50; i++) {
+			System.out.print(".");
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO: handle exception
+				System.out.println("Main thread interrupted.");
+			}
+		}
+		
+		System.out.println("Main thread ending.");
+	}
+
+}
