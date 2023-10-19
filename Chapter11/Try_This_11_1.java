b2849581799480661fb980ef908c846ba27be348
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-19 14:39:07 +0800

    Committed on or around 2023/10/19

--- Chapter11/Try_This_11_1.java
+++ Chapter11/Try_This_11_1.java
@@ -11,13 +11,29 @@ class ExtendThread extends Thread{
 		super(name);
 	}
 	
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println(getName() + " starting.");
+		
+		try {
+			for (int count = 0; count < 10; count++) {
+				Thread.sleep(400);
+				System.out.println("In " + getName() + ", count is " + count);
+			}
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+		}
+		
+		super.run();
+	}
+	
 }
 
 public class Try_This_11_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
