0255b9491093459eb3f3d85169116311b98f678d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-19 17:23:53 +0800

    Committed on or around 2023/10/19

---
+++ Chapter11/Creating_multiple_threads.java
@@ -0,0 +1,22 @@
+// Create multiple threads.
+
+class MyThreads implements Runnable{
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
+
+
+public class Creating_multiple_threads {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
