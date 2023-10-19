38a41806ade7ce2672d1857b299d2d541f252ab5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-19 14:26:58 +0800

    Committed on or around 2023/10/19

---
+++ Chapter11/Try_This_11_1.java
@@ -0,0 +1,19 @@
+/*
+ * Try This 11-1
+ * 
+ * Extend Thread.
+ * 
+ */
+
+class ExtendThread extends Thread{
+	
+}
+
+public class Try_This_11_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
