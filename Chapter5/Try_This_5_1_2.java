c09a78aa6c81b9db452ec3a5bc3a4c1d557fa36b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-21 14:15:17 +0800

    Committed on or around 2023/10/21

---
+++ Chapter5/Try_This_5_1_2.java
@@ -0,0 +1,29 @@
+
+public class Try_This_5_1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] nums = {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};
+		
+		System.out.print("Original array is: ");
+		
+		for (int i = 0; i < nums.length; i++) 
+			System.out.print(" " + nums[i]);
+		System.out.println();
+		
+		// This is the bubble sort.
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = nums.length - 1; j >=i ; j--) {
+				
+				if (nums[j-1] > nums[j]) {									
+					int k = nums[j-1];
+					nums[j-1] = nums[j];
+					nums[j] = k;
+				}
+			}
+		}
+
+	}
+
+}
