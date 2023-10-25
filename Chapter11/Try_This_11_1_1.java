c7ffd180c0433872b28e08aef3f298433168cdb3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-25 18:31:49 +0800

    Committed on or around 2023/10/25

--- Chapter11/Try_This_11_1_1.java
+++ Chapter11/Try_This_11_1_1.java
@@ -4,7 +4,12 @@ class ExtendThread1 extends Thread{
 		// TODO Auto-generated constructor stub
 	}
 	
-	
+	// When a class extends Thread, it must override the run() method, which is the entry point for the new thread,
+	// It must also call start() to begin execution of the new thread.
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
 	
 }
 
