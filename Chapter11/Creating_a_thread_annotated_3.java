d85285b853001bf271704434ad7909f4e60c8437
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-26 12:22:01 +0800

    Committed on or around 2023/10/26

---
+++ Chapter11/Creating_a_thread_annotated_3.java
@@ -0,0 +1,29 @@
+class aptly implements Runnable {
+	
+	String thread_name;
+	
+	public aptly(String thread_name) {
+		
+		this.thread_name = thread_name;
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
+public class Creating_a_thread_annotated_3 {
+
+	// From the main thread, you can create other threads.
+	public static void main(String[] args) {
+		
+		// You create a thread by instantiating an object of type Thread whose constructor encapsulates an object
+	    // that is runnable.
+		
+
+	}
+
+}
