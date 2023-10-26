f10822786322106ea5caf43cb1f0883b670b8d15
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-26 23:00:03 +0800

    Committed on or around 2023/10/26

---
+++ Chapter11/oracle_tutorial/HelloRunnable2.java
@@ -0,0 +1,18 @@
+package oracle_tutorial;
+
+public class HelloRunnable2 implements Runnable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		
+		
+	}
+	
+	
+
+}
