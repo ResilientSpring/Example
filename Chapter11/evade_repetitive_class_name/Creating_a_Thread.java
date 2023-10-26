1b442da8c84bc547e1110336dbc9002c6682ff12
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-26 16:00:12 +0800

    Committed on or around 2023/10/26

--- Chapter11/evade_repetitive_class_name/Creating_a_Thread.java
+++ Chapter11/evade_repetitive_class_name/Creating_a_Thread.java
@@ -43,12 +43,15 @@ public class Creating_a_Thread {
 	// From the main thread, you can create other threads.
 	public static void main(String[] args) {
 		
+		System.out.println("Main thread starting.");
+		
 		// MyThread2023's object is runnable because it inherits run() from Thread class.
 		MyThread2023 myThread2023 = new MyThread2023(1);
 		
 		// Creating a thread by instantiating Thread class. Its constructor encapsulates a runnable object.
 		Thread thread = new Thread(myThread2023, "Child thread");
 		
+		thread.start();		
 
 	}
 
