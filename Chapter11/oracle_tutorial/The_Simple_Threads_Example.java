8266edd913d162121c7e756414418949d49bba83
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-27 15:17:10 +0800

    Committed on or around 2023/10/27

--- Chapter11/oracle_tutorial/The_Simple_Threads_Example.java
+++ Chapter11/oracle_tutorial/The_Simple_Threads_Example.java
@@ -27,14 +27,10 @@ public class The_Simple_Threads_Example {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		// Delay, in milliseconds before
-		// we interrupt MessageLoop
-		// thread (default one hour).
+		// Delay, in milliseconds before we interrupt MessageLoop thread (default one hour).
 		long patience = 1000 * 60 * 60;
 
-		// If command line argument
-		// present, gives patience
-		// in seconds.
+		// If command line argument present, gives patience in seconds.
 		if (args.length > 0) {
 			try {
 				patience = Long.parseLong(args[0]) * 1000;
@@ -50,19 +46,17 @@ public class The_Simple_Threads_Example {
 		t.start();
 
 		threadMessage("Waiting for MessageLoop thread to finish");
-		// loop until MessageLoop
-		// thread exits
+
+		// loop until MessageLoop thread exits
 		while (t.isAlive()) {
 			threadMessage("Still waiting...");
-			// Wait maximum of 1 second
-			// for MessageLoop thread
-			// to finish.
+
+			// Wait maximum of 1 second for MessageLoop thread to finish.
 			t.join(1000);
 			if (((System.currentTimeMillis() - startTime) > patience) && t.isAlive()) {
 				threadMessage("Tired of waiting!");
 				t.interrupt();
-				// Shouldn't be long now
-				// -- wait indefinitely
+				// Shouldn't be long now -- wait indefinitely
 				t.join();
 			}
 		}
