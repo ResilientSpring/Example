b44d3e9ebd4682de25708f73f528c143e2033cce
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-28 23:10:32 +0800

    Committed on or around 2023/11/28

---
+++ Miscellaneous/JavaScript/String_Concatenation.java
@@ -0,0 +1,17 @@
+package JavaScript;
+
+public class String_Concatenation {
+
+	public static void main(String[] args) {
+		
+		String pertinent = "pertain to";
+		
+		String brew = " preamble";
+		
+		String remittance = pertinent + brew;
+		
+		System.out.println(remittance);
+
+	}
+
+}
