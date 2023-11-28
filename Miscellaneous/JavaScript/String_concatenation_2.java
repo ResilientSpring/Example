fc6f310c823d4c8ebbf47a0863029a8e488bec3a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-28 23:13:29 +0800

    Committed on or around 2023/11/28

---
+++ Miscellaneous/JavaScript/String_concatenation_2.java
@@ -0,0 +1,13 @@
+package JavaScript;
+
+public class String_concatenation_2 {
+
+	public static void main(String[] args) {
+		
+		String brew = "Pertaining to " + "preamble";
+		
+		System.out.println(brew);
+
+	}
+
+}
