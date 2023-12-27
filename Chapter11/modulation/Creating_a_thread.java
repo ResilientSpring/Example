4e4c2d9f53acb9d3ba56aee7c7fe10775e6a671d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-27 15:05:48 +0800

    Committed on or around 2023/12/27

--- Chapter11/modulation/Creating_a_thread.java
+++ Chapter11/modulation/Creating_a_thread.java
@@ -1,11 +1,15 @@
 package modulation;
 
 public class Creating_a_thread implements Runnable {
+	
+	String threadName;
 
 	
 	public static void main(String[] args) {
 		
-		new Thread(new Creating_a_thread()).start();		
+		System.out.println("Main thread starting.");
+		
+		new Thread(new Creating_a_thread("Child #1")).start();		
 		
 	}
 
@@ -14,6 +18,11 @@ public class Creating_a_thread implements Runnable {
 		
 		System.out.println();
 		
+	}
+	
+	public Creating_a_thread(String threadName) {
+		
+		this.threadName = threadName;
 	}
 	
 }
