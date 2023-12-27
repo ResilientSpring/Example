a3f371a30e1104d619dbb97152f888e815b853b8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-27 15:35:20 +0800

    Committed on or around 2023/12/27

---
+++ Chapter11/modulation/Creating_a_thread_by_extending_Thread.java
@@ -0,0 +1,38 @@
+package modulation;
+
+public class Creating_a_thread_by_extending_Thread extends Thread {
+	
+	String threadName;
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	
+	@Override
+	public void run() {
+		
+		System.out.println(threadName + " starting.");
+		
+		try {
+			
+			for (int count = 0; count < 10; count++) {
+				Thread.sleep(400);
+				System.out.println("In " + threadName + ", count is " + count);
+			}
+			
+		} catch (InterruptedException exc) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+	public Creating_a_thread_by_extending_Thread(String threadName) {
+		
+		this.threadName = threadName;
+
+	}
+
+}
