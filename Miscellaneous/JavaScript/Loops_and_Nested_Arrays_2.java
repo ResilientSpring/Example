a2a40d0b69e3c5f9b194d25d51d381e354772ce9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-15 00:01:19 +0800

    Committed on or around 2024/02/15

--- Miscellaneous/JavaScript/Loops_and_Nested_Arrays_2.java
+++ Miscellaneous/JavaScript/Loops_and_Nested_Arrays_2.java
@@ -4,22 +4,24 @@ public class Loops_and_Nested_Arrays_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		String[][] students = {{"Thomas", "92", "98"}, {"Roger", "87", "78"}, {"Amber", "81", "85"}, {"Jennifer", "99", "100"}}; 
 
-		for (int i = 0; i < students.length; i++) {
-			
+		String[][] students = { { "Thomas", "92", "98" }, { "Roger", "87", "78" }, { "Amber", "81", "85" },
+				{ "Jennifer", "99", "100" } };
+
+		for (int i = 0; i < students.length; i++)
+
 			for (int j = 0; j < students[i].length; j++) {
-				
-				if (j == 0) {
+
+				if (j == 0)
+
 					System.out.println(students[i][j] + ": ");
-				}else if (j == students[i].length - 1) {
+
+				else if (j == students[i].length - 1)
+
 					System.out.println(students[i][j] + "\n");
-				}
-				
+
 			}
-			
-		}
+
 	}
 
 }
