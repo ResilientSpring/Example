e847f23d383790b88bf3ce36cf5a42fcb4ddf790
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-14 23:48:38 +0800

    Committed on or around 2024/02/14

---
+++ Miscellaneous/JavaScript/Loops_and_Nested_Arrays.java
@@ -0,0 +1,13 @@
+package JavaScript;
+
+public class Loops_and_Nested_Arrays {
+
+	public static void main(String[] args) {
+		
+		String[][] studentStrings = new String[4][3];
+		
+		studentStrings[0][0] = ["Thomas", "92", "90"]; 
+
+	}
+
+}
