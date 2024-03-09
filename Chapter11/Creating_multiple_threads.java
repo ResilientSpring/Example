27780cbd466839274c9b260002529d701ccb5287
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-09 13:40:58 +0800

    Committed on or around 2024.03.09

--- Chapter11/Creating_multiple_threads.java
+++ Chapter11/Creating_multiple_threads.java
@@ -54,15 +54,18 @@ public class Creating_multiple_threads {
 		
 		for (int i = 0; i < 50; i++) {
 			
+			System.out.print(".");
+			
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
-				
-				e.printStackTrace();
+			
+				System.out.println("Main thread interrupted.");
 			}
 			
 		}
 
+		System.out.println("Main thread ending");
 	}
 
 }
