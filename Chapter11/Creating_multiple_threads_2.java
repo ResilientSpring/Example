067b4ab8c52f47548aa56d35640e067b5fa6d161
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-09 14:51:27 +0800

    Committed on or around 2024/03/09

--- Chapter11/Creating_multiple_threads_2.java
+++ Chapter11/Creating_multiple_threads_2.java
@@ -1,12 +1,34 @@
 class OurThread implements Runnable {
 	
+	Thread thread;
+	
 	public OurThread() {
 		
 	}
+	
+	public OurThread(String name) {
+		
+		thread = new Thread(name);
+		
+	}
 
-	@Override
 	public void run() {
 		
+		System.out.println(thread.getName() + " starting.");
+		
+		try {
+			
+			for (int count = 0; count < 10; count++) {
+				
+				Thread.sleep(400);
+				System.out.println("In " + thread.getName() + ", count is " + count);
+			}
+			
+		} catch (InterruptedException e) {
+			System.out.println(thread.getName() + " interrupted.");
+		}
+		
+		System.out.println(thread.getName() + " terminating.");		
 		
 	}
 	
@@ -17,7 +39,12 @@ class OurThread implements Runnable {
 public class Creating_multiple_threads_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Main thread starting.");
+		
+		OurThread ot1 = new OurThread();
+		OurThread ot2 = new OurThread("Child #2");
+		OurThread ot3 = new OurThread("Child #3");
 
 	}
 
