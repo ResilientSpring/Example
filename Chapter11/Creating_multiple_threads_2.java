292b37ae7e2a8cfe1ae1e230a3901baadb901cee
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-10 12:40:22 +0800

    Committed on or around 2024/03/10

--- Chapter11/Creating_multiple_threads_2.java
+++ Chapter11/Creating_multiple_threads_2.java
@@ -45,6 +45,30 @@ public class Creating_multiple_threads_2 {
 		OurThread ot1 = new OurThread();
 		OurThread ot2 = new OurThread("Child #2");
 		OurThread ot3 = new OurThread("Child #3");
+		
+		// OurThread ot4 = new OurThread("Child #4").thread.start();
+		
+		new OurThread("Child #5").thread.start();
+		new OurThread("Child #6").thread.start();
+		
+		OurThread ot7 = new OurThread("Child #7");
+		
+		ot7.thread.start();
+		
+		for (int i = 0; i < 50; i++) {
+			
+			System.out.print(".");
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+			
+				System.out.println("Main thread interrupted.");
+			}
+			
+		}
+
+		System.out.println("Main thread ending");
 
 	}
 
