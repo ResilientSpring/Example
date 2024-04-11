7adbf87832f04dcdc3d965de0f1ca8bb2b59a874
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-11 23:17:30 +0800

    Committed on or around 2024/04/11

---
+++ Chapter6/refresher/NestedClassDemo3.java
@@ -0,0 +1,62 @@
+package refresher;
+
+
+class Outer3 {
+
+	int nums[];
+
+	Outer3(int n[]) {
+		// TODO Auto-generated constructor stub
+		nums = n;
+	}
+
+	void analyze() {
+		Inner inOb = new Inner();
+
+		System.out.println("Minimum: " + inOb.min());
+		System.out.println("Maximum: " + inOb.max());
+		System.out.println("Average: " + inOb.avg());
+	}
+
+	// This is an inner class.
+	class Inner {
+		
+		int min() {
+			int m = nums[0];
+
+			for (int i = 1; i < nums.length; i++)
+				if (nums[i] < m)
+					m = nums[i];
+			return m;
+		}
+
+		int max() {
+			int m = nums[0];
+			for (int i = 1; i < nums.length; i++)
+				if (nums[i] > m)
+					m = nums[i];
+
+			return m;
+		}
+
+		int avg() {
+			int a = 0;
+			for (int i = 0; i < nums.length; i++)
+				a += nums[i];
+
+			return a / nums.length;
+		}
+	}
+}
+
+public class NestedClassDemo3 {
+
+	public static void main(String[] args) {
+		
+		int[] x = {2, 4, 6, 8, 10};
+		
+		Outer3 outer3 = new Outer3(x);
+
+	}
+
+}
