49c5215d5b2e591c4638fa102209c97b66e440b0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-11 13:36:40 +0800

    Committed on or around 2024/04/11

--- Chapter6/refresher/NestedClassDemo.java
+++ Chapter6/refresher/NestedClassDemo.java
@@ -1,5 +1,7 @@
 package refresher;
 
+import refresher.Outer.Inner;
+
 class Outer {
 
 	int nums[];
@@ -54,9 +56,12 @@ public class NestedClassDemo {
 	public static void main(String[] args) {
 		
 		int x[] = { 3, 2, 1, 5, 6, 9, 7, 8 };
+		
 		Outer outOb = new Outer(x);
 
 		outOb.analyze();
+		
+		Inner inner = new Inner();
 
 	}
 
