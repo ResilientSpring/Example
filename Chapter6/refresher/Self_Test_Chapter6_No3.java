833f4b0b813642fdf4e448f3d5681ba2416e04f1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-30 01:42:18 +0800

    Stack from the author's solution.

---
+++ Chapter6/refresher/Self_Test_Chapter6_No3.java
@@ -0,0 +1,17 @@
+package refresher;
+
+class Stack{
+	
+	private char stck[];   // this array holds the stack.
+	private int tos;    // top of stack
+	
+}
+
+public class Self_Test_Chapter6_No3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
