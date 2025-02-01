c9f52b584438c04db84b1d2554a04398c17b3371
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-02-01 16:33:47 +0800

    Committed on or around 2025/02/01

---
+++ Miscellaneous/Cpp/Array_of_objects_3.java
@@ -0,0 +1,38 @@
+package Cpp;
+
+class MyClasses{
+	private int x;
+	
+	void set_x(int i) {
+		x = i;
+	}
+	
+	int get_x() {
+		return x;
+	}
+}
+
+public class Array_of_objects_3 {
+
+	public static void main(String[] args) {
+		
+		
+		MyClasses[] objects = new MyClasses[4];
+		
+		objects[0] = new MyClasses();
+		objects[0].set_x(11);
+		
+		objects[1].set_x(22);
+		
+		objects[2].set_x(33);
+		
+		
+//		for (int i = 0; i < objects.length; i++) 
+//			objects[i].set_x(i);
+		
+		for (int i = 0; i < objects.length; i++) 
+			System.out.println("Objects[" + i + "].get_x(): " + objects[i].get_x());
+
+	}
+
+}
