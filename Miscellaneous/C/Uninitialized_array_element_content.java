7ca224008e6c9799de035eb0818a1a782f2edc83
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-05-27 14:00:22 +0800

    Committed on or around 2025/05/27

---
+++ Miscellaneous/C/Uninitialized_array_element_content.java
@@ -0,0 +1,13 @@
+package C;
+
+public class Uninitialized_array_element_content {
+
+	public static void main(String[] args) {
+		
+		final int ALL_INPUT_LINES_LONGER_THAN_EIGHTY = 500; 
+		
+		char more_than_80_characters[] = new char[ALL_INPUT_LINES_LONGER_THAN_EIGHTY];
+
+	}
+
+}
