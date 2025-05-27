b94649a14d2bb6d09459f438a683ea89e43311ce
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-05-27 14:11:46 +0800

    Committed on or around 2025/05/27

--- Miscellaneous/C/Uninitialized_array_element_content.java
+++ Miscellaneous/C/Uninitialized_array_element_content.java
@@ -7,6 +7,13 @@ public class Uninitialized_array_element_content {
 		final int ALL_INPUT_LINES_LONGER_THAN_EIGHTY = 500; 
 		
 		char more_than_80_characters[] = new char[ALL_INPUT_LINES_LONGER_THAN_EIGHTY];
+		
+		if(more_than_80_characters[0] == 0)
+			System.out.println("more_than_80_characters[0] is 0\n");
+		else 
+			
+			System.out.println("more_than_80_characters[0] is " + more_than_80_characters[0]);
+		
 
 	}
 
