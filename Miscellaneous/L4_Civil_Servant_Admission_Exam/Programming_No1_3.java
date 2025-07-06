4efbf3e3cb304ab61e3fc9a7c68647ca5abc8ac2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-07-06 19:52:30 +0800

    Committed on or around 2025/07/06

---
+++ Miscellaneous/L4_Civil_Servant_Admission_Exam/Programming_No1_3.java
@@ -0,0 +1,32 @@
+package L4_Civil_Servant_Admission_Exam;
+
+public class Programming_No1_3 {
+	
+	static long Catalan_Number(int n) {
+
+		long sum;
+
+		if (n == 0) {
+
+			sum = 1;
+
+			return sum;
+
+		}
+
+		for (int i = 0; i < n; i++) {
+
+			sum = Catalan_Number(i) * Catalan_Number(n - 1 - i);
+
+		}
+
+		return sum;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
